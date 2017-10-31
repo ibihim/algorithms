d23a8c3ab32b4e48d8791455909ece97c981a3e2
Krzysztof Ostrowski <ibihim@posteo.de>
2017-11-01 00:03:55 +0100

    working on n square sorts 2 - removing only from test

--- tests/sorting/selection-sort-test.js
+++ tests/sorting/selection-sort-test.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const selectionSort = require('../../sorting/selection-sort');
 
 describe('selection sort', () => {
-    it.only('should sort items', () => {
+    it('should sort items', () => {
         const given = [4, 3, 2, 1];
         const expected = [1, 2, 3, 4];
 
